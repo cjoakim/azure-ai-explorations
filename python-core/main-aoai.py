64e5b3c9e094866184fe798d23fade48152559ed
Chris Joakim <christopher.joakim@gmail.com>
2025-06-12 17:46:50 -0400

    python-core/main-aoai.py list models

--- python-core/main-aoai.py
+++ python-core/main-aoai.py
@@ -5,12 +5,19 @@ Usage:
   python main-aoai.py explore
 """
 
+# See https://github.com/Azure-Samples/openai
+# See https://learn.microsoft.com/en-us/azure/ai-services/openai/quickstart?tabs=command-line%2Ckeyless%2Ctypescript-keyless%2Ckey&pivots=programming-language-python
+# https://pypi.org/project/openai/
+# https://github.com/openai/openai-python
+
 import sys
 import os
 import traceback
 
 from docopt import docopt
 from dotenv import load_dotenv
+
+from openai import AzureOpenAI
 
 from src.io.fs import FS
 from src.os.env import Env
@@ -25,7 +32,26 @@ def print_options(msg):
 
 
 def explore():
-    pass
+    url = os.environ.get("AZURE_OPENAI_URL")
+    key = os.environ.get("AZURE_OPENAI_KEY")
+    print("Azure OpenAI URL:", url)
+    print("Azure OpenAI Key:", key)
+
+    client = AzureOpenAI(
+        api_key=key,  
+        api_version="2024-10-21",
+        azure_endpoint = url)
+    print(str(type(client)))  # <class 'openai.lib.azure.AzureOpenAI'>
+    print(client)
+
+    try:
+        models = client.models.list()
+        for midx, model in enumerate(models.data):
+            print(f"=== model {midx} {model.id}")
+            print(model)
+        print(f"Total models: {len(models.data)}")
+    except Exception as e:
+        print(f"An error occurred: {e}")
 
 if __name__ == "__main__":
     try:
