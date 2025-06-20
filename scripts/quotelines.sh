303ba67343d5a7fc138dcc21363eccb40b52f716
Chris Joakim <christopher.joakim@gmail.com>
2025-06-20 16:44:12 -0400

    Updated comments in scripts/quotelines.sh

--- scripts/quotelines.sh
+++ scripts/quotelines.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-# Utility script to somewhat a transform the list of dependencies
-# in a requirements.txt file to a quoted format for a corresponding
-# pyproject.toml file.
+# Utility script to somewhat transform the list of dependencies in a
+# requirements.txt file to a quoted format for a corresponding pyproject.toml
+# file.  Minimizes the manual work of quoting each requirement.
 #
 # Usage: ./quotelines.sh requirements.in > pyproject.toml
 #
